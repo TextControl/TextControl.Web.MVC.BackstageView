dc3e145c121066cd11e759e105a55da96ba9ec46
bjoerntx <bjoern@textcontrol.com>
2015-12-30 13:00:51 +0100

    Uploaded sample

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tx_backstage_view.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetView(string viewName)
+        {
+            return PartialView(viewName);
+        }
+    }
+}
